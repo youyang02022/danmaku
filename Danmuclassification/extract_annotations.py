355a0897e003872f399dac1989c0c8d3e27f676d
youyang02022 <youyang.peng@umontreal.ca>
2022-06-10 12:19:11 -0400

    cleaning

--- Danmuclassification/extract_annotations.py
+++ Danmuclassification/extract_annotations.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 with open(
     "ann.json/master/pool/aI8Fw.Pydo23hL3pCu8Vt66e7jAO-Danmaku_______.txt.ann.json",
@@ -9,7 +10,12 @@ with open(
     for entry in annotations['entities']:
         summary.append({'class_id': entry['classId'], 'text': entry['offsets'][0]['text']})
 
-    #print('\n'.join([str(d) for d in summary]))
+    summary = re.sub('31*','', 'text': entry['offsets'][0]['text'])
+
+
+
+
+        print('\n'.join([str(d) for d in summary]))
 
     with open("sample.csv", "w", encoding="UTF-8") as out_f:
         out_f.write("class_id\ttext\n")
